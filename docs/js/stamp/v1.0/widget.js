20d8167c24a96152d14842fe205f5f38b23689f7
shapoco <40152459+shapoco@users.noreply.github.com>
2024-10-09 01:38:24 +0900

    fix stamp cookie

--- docs/js/stamp/v1.0/widget.js
+++ docs/js/stamp/v1.0/widget.js
@@ -122,7 +122,7 @@ class ShapocoNetStamp {
     }
     if (resp.clientId) {
       this.clientId = resp.clientId;
-      document.cookie = `${ShapocoNetStamp.COOKIE_KEY}=${encodeURIComponent(this.clientId)}; max-age=${86400 * 365}; SameSite=Lax; Secure`;
+      document.cookie = `${ShapocoNetStamp.COOKIE_KEY}=${encodeURIComponent(this.clientId)}; Path=/; max-age=${86400 * 365}; SameSite=Lax; Secure`;
     }
     if (resp.commentRule) {
       this.commentRule = resp.commentRule;
